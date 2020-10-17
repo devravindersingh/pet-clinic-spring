49c7973ab7cd52b93d387161b772115b5c3c5e28
devravindersingh <ravindersngh402@outlook.com>
2020-10-17 16:29:37 +0530

    common base interface . closes #11

--- pet-clinic-data/src/main/java/com/learning/ravinder/service/VetService.java
+++ pet-clinic-data/src/main/java/com/learning/ravinder/service/VetService.java
@@ -2,10 +2,6 @@ package com.learning.ravinder.service;
 
 import com.learning.ravinder.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
