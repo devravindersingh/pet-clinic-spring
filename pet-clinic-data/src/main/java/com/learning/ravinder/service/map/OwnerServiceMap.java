6eb6a8d0582ff566cae6bce43782bb1313b99093
devravindersingh <ravindersngh402@outlook.com>
2020-10-17 16:59:22 +0530

    map based service implemented . closes #6

---
+++ pet-clinic-data/src/main/java/com/learning/ravinder/service/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.learning.ravinder.service.map;
+
+import com.learning.ravinder.model.Owner;
+import com.learning.ravinder.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.deleteByObject(object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
