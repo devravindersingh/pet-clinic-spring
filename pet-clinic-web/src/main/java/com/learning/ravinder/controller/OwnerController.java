918642bcef47f31f128abe380542f1efa9cc569a
devravindersingh <ravindersngh402@outlook.com>
2020-10-19 22:51:18 +0530

    list all owner on owner index page . closes #16

--- pet-clinic-web/src/main/java/com/learning/ravinder/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/learning/ravinder/controller/OwnerController.java
@@ -1,14 +1,24 @@
 package com.learning.ravinder.controller;
 
+import com.learning.ravinder.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owner")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping("/index")
-    public String listOwners(){
+    public String listOwners(Model owners){
+
+        owners.addAttribute("ownerList", ownerService.findAll());
         return "owners/index";
     }
 
