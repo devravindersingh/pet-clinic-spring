494517d7326132cc1bcddb58d552570bb3441fe1
devravindersingh <ravindersngh402@outlook.com>
2020-10-19 22:57:58 +0530

    list all vet on vet index page . closes #15

--- pet-clinic-web/src/main/java/com/learning/ravinder/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/learning/ravinder/controller/VetController.java
@@ -1,6 +1,8 @@
 package com.learning.ravinder.controller;
 
+import com.learning.ravinder.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"/vets", "/vets/index"})
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping
-    public String listVets(){
+    public String listVets(Model vets){
+        vets.addAttribute("vetList", vetService.findAll());
         return "vets/index";
     }
 }
