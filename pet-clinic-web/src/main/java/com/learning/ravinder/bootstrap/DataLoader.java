1433cea3e32e31d89e13b2159b38535b3a58b565
devravindersingh <ravindersngh402@outlook.com>
2020-10-21 14:11:45 +0530

    petType added in DataLoader for bootstrap closes #32

--- pet-clinic-web/src/main/java/com/learning/ravinder/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/learning/ravinder/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.learning.ravinder.bootstrap;
 
 import com.learning.ravinder.model.Owner;
+import com.learning.ravinder.model.PetType;
 import com.learning.ravinder.model.Vet;
 import com.learning.ravinder.service.OwnerService;
+import com.learning.ravinder.service.PetTypeService;
 import com.learning.ravinder.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
