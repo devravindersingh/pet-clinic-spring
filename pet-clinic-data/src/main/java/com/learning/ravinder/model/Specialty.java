3dada51b6d45097ed510c2b639df01eefac30093
devravindersingh <ravindersngh402@outlook.com>
2020-10-24 17:51:40 +0530

    Updated Visit with JPA annotations. closes #38

--- pet-clinic-data/src/main/java/com/learning/ravinder/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/learning/ravinder/model/Specialty.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "specialties")
 public class Specialty extends BaseEntity{
+
     @Column(name = "description")
     private String description;
 
