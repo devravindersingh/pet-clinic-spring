f380c3a74c4e654fa8f4b4c5e5ac647a4abde658
devravindersingh <ravindersngh402@outlook.com>
2020-10-25 21:27:23 +0530

    Visit Map Service . closes #46

---
+++ pet-clinic-data/src/main/java/com/learning/ravinder/service/VisitService.java
@@ -0,0 +1,6 @@
+package com.learning.ravinder.service;
+
+import com.learning.ravinder.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/learning/ravinder/service/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.learning.ravinder.service.map;
+
+import com.learning.ravinder.model.Visit;
+import com.learning.ravinder.service.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.deleteByObject(object);
+    }
+
+
+}
